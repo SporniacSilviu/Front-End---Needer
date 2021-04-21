e913875d2a6ac3222302447659d1760327c5e316
SporniacSilviu <79246954+SporniacSilviu@users.noreply.github.com>
2021-04-21 14:10:26 +0300

    Add files via upload

---
+++ helppage/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-bg">
+          <h2 align="center" style={{ color:"#b83b5e" }}>Bine ai venit pe site! </h2>
+          <h4 align="center" style={{ color: "#F5F5F5"}}>Singuri suntem doar o picatură, dar împreună suntem oceanul. </h4>
+          <h3 align="center" style={{ color: 'white' }}> Impreuna ne dorim sa cream o comunitate unita si sa putem rezolva cat mai multe probleme!</h3>
+          <p align="center" style={{ color: 'white' }}>Ai nevoie de ajutor? Esti unde trebuie! Tot ce trebuie sa faci este sa completezi acest formular. Dupa finalizarea acestuia vei fi redirectionat intr-o camera de asteptare unde vei fi preluat de o persoana care te poate ajuta.</p>
+
+        <button type="button" className="form-button">Formular Nevoi!</button>
+      </header>
+    </div>
+  );
+}
+export default App;
